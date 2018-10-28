409d8292bd0f6f6622fc9d4ee982cbc734c2b27e
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2018-10-28 09:25:32 +0100

    Add dependency to JUnit5

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,15 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+val junitVersion = "5.3.1"
+
+dependencies {
+    implementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    implementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+tasks.withType(Test::class) {
+    useJUnitPlatform()
+}
